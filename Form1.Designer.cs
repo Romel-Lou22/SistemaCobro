156753d5ea4d049d7fc8f5ff492abbc037b6ba83
Romel-Lou22 <romelalba@outlook.es>
2024-10-02 09:46:01 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace SistemaCobro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCobrar = new System.Windows.Forms.Button();
+            this.btnComprobante = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Date = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.RbMensual = new System.Windows.Forms.RadioButton();
+            this.RbAnual = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese:";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(227, 76);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(307, 26);
+            this.txtBuscar.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(604, 69);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(88, 33);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombres:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Forma Pago:";
+            // 
+            // btnCobrar
+            // 
+            this.btnCobrar.Location = new System.Drawing.Point(213, 370);
+            this.btnCobrar.Name = "btnCobrar";
+            this.btnCobrar.Size = new System.Drawing.Size(75, 38);
+            this.btnCobrar.TabIndex = 5;
+            this.btnCobrar.Text = "Cobrar";
+            this.btnCobrar.UseVisualStyleBackColor = true;
+            this.btnCobrar.Click += new System.EventHandler(this.btnCobrar_Click);
+            // 
+            // btnComprobante
+            // 
+            this.btnComprobante.Location = new System.Drawing.Point(407, 370);
+            this.btnComprobante.Name = "btnComprobante";
+            this.btnComprobante.Size = new System.Drawing.Size(142, 38);
+            this.btnComprobante.TabIndex = 6;
+            this.btnComprobante.Text = "Comprobante";
+            this.btnComprobante.UseVisualStyleBackColor = true;
+            this.btnComprobante.Click += new System.EventHandler(this.btnComprobante_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Fecha:";
+            // 
+            // Date
+            // 
+            this.Date.Location = new System.Drawing.Point(227, 286);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(307, 26);
+            this.Date.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(93, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Codigo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(93, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Cedula:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(227, 115);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.ReadOnly = true;
+            this.txtCodigo.Size = new System.Drawing.Size(307, 26);
+            this.txtCodigo.TabIndex = 11;
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(227, 156);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.ReadOnly = true;
+            this.txtCedula.Size = new System.Drawing.Size(307, 26);
+            this.txtCedula.TabIndex = 12;
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(227, 195);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.ReadOnly = true;
+            this.txtNombres.Size = new System.Drawing.Size(360, 26);
+            this.txtNombres.TabIndex = 13;
+            // 
+            // RbMensual
+            // 
+            this.RbMensual.AutoSize = true;
+            this.RbMensual.Location = new System.Drawing.Point(227, 230);
+            this.RbMensual.Name = "RbMensual";
+            this.RbMensual.Size = new System.Drawing.Size(94, 24);
+            this.RbMensual.TabIndex = 14;
+            this.RbMensual.TabStop = true;
+            this.RbMensual.Text = "Mensual";
+            this.RbMensual.UseVisualStyleBackColor = true;
+            // 
+            // RbAnual
+            // 
+            this.RbAnual.AutoSize = true;
+            this.RbAnual.Location = new System.Drawing.Point(423, 232);
+            this.RbAnual.Name = "RbAnual";
+            this.RbAnual.Size = new System.Drawing.Size(75, 24);
+            this.RbAnual.TabIndex = 15;
+            this.RbAnual.TabStop = true;
+            this.RbAnual.Text = "Anual";
+            this.RbAnual.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(665, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "TEST";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RbAnual);
+            this.Controls.Add(this.RbMensual);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnComprobante);
+            this.Controls.Add(this.btnCobrar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Sistema Cobro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCobrar;
+        private System.Windows.Forms.Button btnComprobante;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker Date;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.RadioButton RbMensual;
+        private System.Windows.Forms.RadioButton RbAnual;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace SistemaCobro
+{
+    public partial class Form1 : Form
+    {
+        // Cadena de conexión a tu base de datos
+        private string connectionString = ConfigurationManager.ConnectionStrings["miConexion"].ConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Evento al cargar el formulario
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Método para probar la conexión a la base de datos
+        private void TestConnection()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open(); // Abre la conexión
+                    MessageBox.Show("Conexión exitosa a la base de datos.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (SqlException ex)
+            {
+                // Manejo de errores SQL
+                MessageBox.Show($"Error SQL al conectar a la base de datos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores generales
+                MessageBox.Show($"Error al conectar a la base de datos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            TestConnection();
+        }
+
+        //BTNBUSCAR
+        private void btnBuscar_Click_1(object sender, EventArgs e)
+        {
+            string query = "";
+
+            // Check if the search text is a number (to search by CodigoUsuario or Cedula)
+            bool isNumeric = long.TryParse(txtBuscar.Text, out _);
+
+            // Modify the query based on the input type
+            if (isNumeric)
+            {
+                query = "SELECT CodigoUsuario, Cedula, UsuarioSistema FROM Usuarios WHERE Cedula = @cedula OR CodigoUsuario = @codigo";
+            }
+            else
+            {
+                query = "SELECT CodigoUsuario, Cedula, UsuarioSistema FROM Usuarios WHERE UsuarioSistema LIKE @nombres";
+            }
+
+            // Connect to the database
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    if (isNumeric)
+                    {
+                        // Set parameters for numeric search
+                        cmd.Parameters.Add(new SqlParameter("@cedula", System.Data.SqlDbType.VarChar) { Value = txtBuscar.Text });
+                        cmd.Parameters.Add(new SqlParameter("@codigo", System.Data.SqlDbType.BigInt) { Value = Convert.ToInt64(txtBuscar.Text) });
+                    }
+                    else
+                    {
+                        // Set parameters for name search
+                        cmd.Parameters.Add(new SqlParameter("@nombres", System.Data.SqlDbType.NVarChar) { Value = "%" + txtBuscar.Text + "%" });
+                    }
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // Check if any results were found
+                    if (reader.Read())
+                    {
+                        txtCodigo.Text = reader["CodigoUsuario"].ToString();
+                        txtCedula.Text = reader["Cedula"].ToString();
+                        txtNombres.Text = reader["UsuarioSistema"].ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se encontraron resultados.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Error SQL: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void btnCobrar_Click(object sender, EventArgs e)
+        {
+            // Verificar el tipo de pago y asignar el monto correspondiente
+            decimal montoPago = 0;
+            string tipoPago = "";
+
+            if (RbMensual.Checked)
+            {
+                montoPago = 10;
+                tipoPago = "Mensual";
+            }
+            else if (RbAnual.Checked)
+            {
+                montoPago = 120;
+                tipoPago = "Anual";
+            }
+            else
+            {
+                MessageBox.Show("Por favor, seleccione un tipo de pago.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Salir del método si no se seleccionó un tipo de pago
+            }
+
+            // Obtener los valores de los TextBox
+            string codigoUsuario = txtCodigo.Text;
+            string cedula = txtCedula.Text;
+            string nombres = txtNombres.Text;
+            DateTime fechaPago = Date.Value; // Usar la fecha seleccionada en el DateTimePicker
+
+            // Insertar los datos en la base de datos
+            string query = "INSERT INTO Pagos (CodigoUsuario, Cedula, UsuarioSistema, FechaPago, MontoPago, TipoPago) " +
+                           "VALUES (@codigoUsuario, @cedula, @nombres, @fechaPago, @montoPago, @tipoPago)";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open(); // Abrir la conexión
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    // Añadir los parámetros para la consulta
+                    cmd.Parameters.AddWithValue("@codigoUsuario", codigoUsuario);
+                    cmd.Parameters.AddWithValue("@cedula", cedula);
+                    cmd.Parameters.AddWithValue("@nombres", nombres);
+                    cmd.Parameters.AddWithValue("@fechaPago", fechaPago); // Usar la fecha del DateTimePicker
+                    cmd.Parameters.AddWithValue("@montoPago", montoPago);
+                    cmd.Parameters.AddWithValue("@tipoPago", tipoPago);
+
+                    // Ejecutar la consulta de inserción
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Pago registrado exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se pudo registrar el pago. Intente nuevamente.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    // Manejo de errores específicos de SQL
+                    MessageBox.Show("Error SQL: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    // Manejo de otros errores
+                    MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnComprobante_Click(object sender, EventArgs e)
+        {
+            // Obtener los valores necesarios para generar el comprobante
+            string codigoUsuario = txtCodigo.Text;
+            string cedula = txtCedula.Text;
+            string nombres = txtNombres.Text;
+            DateTime fechaPago = Date.Value;
+            decimal montoPago = RbMensual.Checked ? 10 : 120;
+            string tipoPago = RbMensual.Checked ? "Mensual" : "Anual";
+
+            // Especificar la ruta base donde se guardarán los comprobantes
+            string rutaBase = @"C:\Users\ZenBook\Desktop\Comp";
+
+            // Asegurarse de que la carpeta exista
+            Directory.CreateDirectory(rutaBase);
+
+            // Crear el nombre del archivo PDF
+            string nombreArchivo = $"Comprobante_{cedula}_{DateTime.Now:yyyyMMddHHmmss}.pdf";
+            string rutaCompleta = Path.Combine(rutaBase, nombreArchivo);
+
+            try
+            {
+                // Crear el documento PDF con tamaño A4
+                using (Document documento = new Document(PageSize.A4, 50, 50, 25, 25))
+                {
+                    PdfWriter.GetInstance(documento, new FileStream(rutaCompleta, FileMode.Create));
+                    documento.Open();
+
+                    // Definir fuentes
+                    BaseFont bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+                    Font fuenteTitulo = new Font(bf, 16, iTextSharp.text.Font.BOLD);
+                    Font fuenteNormal = new Font(bf, 12, iTextSharp.text.Font.NORMAL);
+                    Font fuenteNegrita = new Font(bf, 12, iTextSharp.text.Font.BOLD);
+
+                    // Agregar el encabezado
+                    Paragraph titulo = new Paragraph("J.A.A.P.P", fuenteTitulo);
+                    titulo.Alignment = Element.ALIGN_CENTER;
+                    documento.Add(titulo);
+
+                    Paragraph subtitulo = new Paragraph("JUNTA ADMINISTRADORA AGUA POTABLE DE PILACOTO", fuenteNegrita);
+                    subtitulo.Alignment = Element.ALIGN_CENTER;
+                    documento.Add(subtitulo);
+
+                    // Función para agregar párrafos centrados
+                    void AgregarParrafoCentrado(string texto)
+                    {
+                        Paragraph p = new Paragraph(texto, fuenteNormal);
+                        p.Alignment = Element.ALIGN_CENTER;
+                        documento.Add(p);
+                    }
+
+                    AgregarParrafoCentrado("R.U.C: 0591763007001");
+                    AgregarParrafoCentrado("PROVINCIA: COTOPAXI CANTON: LATACUNGA");
+                    AgregarParrafoCentrado("CIUDAD: PILACOTO COMUNA: PILACOTO");
+                    AgregarParrafoCentrado("DIRECCIÓN: PILACOTO");
+                    AgregarParrafoCentrado("TELEFONO.: 03-2690779");
+                    documento.Add(new Paragraph("\n"));
+
+                    // Agregar los detalles del pago
+                    Paragraph tituloPago = new Paragraph("Comprobante de Pago", fuenteNegrita);
+                    tituloPago.Alignment = Element.ALIGN_CENTER;
+                    documento.Add(tituloPago);
+
+                    documento.Add(new Paragraph("-----------------------------------------------------------------------", fuenteNormal));
+
+                    // Función para agregar detalles con alineación y formato
+                    void AgregarDetalle(string etiqueta, string valor)
+                    {
+                        Paragraph p = new Paragraph();
+                        p.Add(new Chunk(etiqueta + ": ", fuenteNegrita));
+                        p.Add(new Chunk(valor, fuenteNormal));
+                        documento.Add(p);
+                    }
+
+                    AgregarDetalle("Código Usuario", codigoUsuario);
+                    AgregarDetalle("Cédula", cedula);
+                    AgregarDetalle("Nombres", nombres);
+                    AgregarDetalle("Fecha de Pago", fechaPago.ToShortDateString());
+                    AgregarDetalle("Tipo de Pago", tipoPago);
+                    AgregarDetalle("Monto Pagado", montoPago.ToString("C"));
+
+                    documento.Add(new Paragraph("-----------------------------------------------------------------------", fuenteNormal));
+
+                    Paragraph gracias = new Paragraph("Gracias por su pago.", fuenteNegrita);
+                    gracias.Alignment = Element.ALIGN_CENTER;
+                    documento.Add(gracias);
+                }
+
+                // Confirmar al usuario que el comprobante fue generado
+                MessageBox.Show($"Comprobante PDF generado exitosamente.\nRuta: {rutaCompleta}", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Abrir el PDF generado
+                System.Diagnostics.Process.Start(rutaCompleta);
+            }
+            catch (Exception ex)
+            {
+                // Manejar errores durante la generación del PDF
+                MessageBox.Show($"Error al generar el comprobante PDF: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
